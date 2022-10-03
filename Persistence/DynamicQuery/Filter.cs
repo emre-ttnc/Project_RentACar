70bfe58d309fe27a338d6143bbe6bd6cbf2caccd
Emre <emre.tutuncu@hotmail.com.tr>
2022-10-04 02:39:17 +0300

    Dynamic Query Added.

---
+++ Persistence/DynamicQuery/Filter.cs
@@ -0,0 +1,24 @@
+﻿namespace Persistence.DynamicQuery;
+
+public class Filter
+{
+    public string Field { get; set; }
+    public string Operator { get; set; }
+
+    public string? Value { get; set; }
+    public string? Logic { get; set; }
+    public IEnumerable<Filter>? Filters { get; set; }
+
+    public Filter()
+    {
+    }
+
+    public Filter(string filed, string @operator, string? value, string? logic, IEnumerable<Filter>? filters)
+    {
+        Field = filed;
+        Operator = @operator;
+        Value = value;
+        Logic = logic;
+        Filters = filters;
+    }
+}
